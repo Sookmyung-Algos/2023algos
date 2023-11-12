92bb1421ed27aacaceb2b2524aaf5b75264bf8f9
Yeina Lee <84684759+Yeinalee@users.noreply.github.com>
2023-11-12 00:13:35 -0500

    Create 14848.cpp

---
+++ algos_assignment/이에나_njs07081/fall/week3/14848.cpp
@@ -0,0 +1,57 @@
+using namespace std;
+using namespace __gnu_cxx;
+ 
+using ll = long long;
+using pii = pair<int, int>;
+ 
+int n, k;
+vector<int> v;
+int ans;
+ 
+void solve()
+{
+  cin >> n >> k;
+  ans = n;
+  v.resize(k);
+ 
+  for (int i = 0; i < k; i++)
+  {
+    cin >> v[i];
+    if (v[i] == 1)
+    {
+      cout << 0;
+      return;
+    }
+  }
+ 
+  int mask = (1 << k) - 1;
+  while (mask)
+  {
+    int cnt = __builtin_popcount(mask);
+    ll val = 1;
+    for (int i = 0; i < k; i++)
+    {
+      if (mask & (1 << i))
+      {
+        val = lcm(val, v[i]);
+      }
+    }
+ 
+    if (cnt & 1) ans -= n / val;
+    else ans += n / val;
+    --mask;
+  }
+  cout << ans;
+}
+ 
+int main()
+{
+#ifdef LOCAL
+  freopen("input.txt", "r", stdin);
+//  freopen("output.txt", "w", stdout);
+#endif
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+ 
+  solve();
+}
