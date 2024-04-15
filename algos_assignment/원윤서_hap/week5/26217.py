6c4b87060cb832747e18f1556cd89c23f27747a0
hunt-and-peck <102416954+hunt-and-peck@users.noreply.github.com>
2024-04-15 21:04:43 +0900

    Delete algos_assignment/원윤서_hap directory

--- algos_assignment/원윤서_hap/week5/26217.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-answer = 0
-
-for i in range(1, n+1):
-    answer += (n/i)
-print(answer)
-
-# 출처: https://acupoframen.tistory.com/49
--- algos_assignment/원윤서_hap/week9/16916.py
+++
@@ -1,8 +0,0 @@
-s = input()
-p = input()
-if p in s:
-	print(1)
-else:
-	print(0)
-
-# 출처: https://imzzan.tistory.com/109
